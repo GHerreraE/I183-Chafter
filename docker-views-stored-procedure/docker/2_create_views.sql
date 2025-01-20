bd36849049f7c03c05f17e52fe59313e67d68ba8
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-01-20 15:42:56 +0100

    okey 2

--- docker-views-stored-procedure/docker/2_create_views.sql
+++ docker-views-stored-procedure/docker/2_create_views.sql
@@ -5,5 +5,20 @@ JOIN t_course
 ON t_student.idStudent = t_course.idCourse 
 JOIN t_evaluation 
 ON t_student.idStudent = t_evaluation.idEvaluation;
+CREATE VIEW v_studentsBadGrades AS
+SELECT stuName, stuFirstName, courName, evaGrade, evaDate 
+FROM t_student 
+JOIN t_course 
+ON t_student.idStudent = t_course.idCourse 
+JOIN t_evaluation 
+ON t_student.idStudent = t_evaluation.idEvaluation
+WHERE evaGrade < 4;
+CREATE VIEW v_absentStudents AS
+SELECT stuName, stuFirstName, absDate, absPeriodStart, absPeriodEnd, reaDescription
+FROM t_student
+JOIN t_absence
+ON t_student.idStudent = t_absence.idAbsence
+JOIN t_reason
+ON t_student.idStudent = t_reason.idReason;
 
 
--- docker-views-stored-procedure/docker/3_create_users.sql
+++ docker-views-stored-procedure/docker/3_create_users.sql
@@ -1,2 +1,4 @@
 CREATE USER 'teacher'@'%' IDENTIFIED BY 'root';
 GRANT SELECT ON v_studentsGrades TO 'teacher'@'%';
+GRANT SELECT ON v_studentsBadGrades TO 'teacher'@'%';
+GRANT SELECT ON v_absentStudents TO 'teacher'@'%';
